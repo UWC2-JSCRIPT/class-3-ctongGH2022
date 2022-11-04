cdd00e6f9f074b835a64451db20739cf70a1f3c2
Cecilia <ctong10@uw.edu>
2022-11-04 13:11:23 -0700

    complete Spaceships homework

--- spaceShip.js
+++ spaceShip.js
@@ -1,9 +1,30 @@
+/**
+ * Cecilia Tong
+ * Homework #3
+ * Tuesday 11/1/2022
+ * Spaceships
+ */
+
 // 1. Create a class function SpaceShip
 // - should set two properties: name and topSpeed
 // - should have a method accelerate that logs to the console 
 //   `${name} moving to ${topSpeed}`
 
-
+//define class
+class SpaceShip {
+    constructor(name, topSpeed){
+        this.name = name;
+        this.topSpeed = topSpeed;
+    }
+accelerate(){
+    const {name, topSpeed} = this;
+    console.log(`${name} moving to ${this.topSpeed}`);
+};
+}
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
+const imaginationShip = new SpaceShip('Imagination', '163 km/s');
+const libertyShip = new SpaceShip('Liberty', '150 km/s');
+libertyShip.accelerate();
+imaginationShip.accelerate();
