070704979654be53e18d8e492dd5d0bdbbf3e38b
Cecilia <ctong10@uw.edu>
2022-11-05 22:34:00 -0700

    completed Soccer Standing hw

--- soccer.js
+++ soccer.js
@@ -5,24 +5,6 @@
  * Soccer Standing
  */
 
-/**
-Create a function getTotalPoints that accepts a results string with ‘w’ for a win, 
-‘l’ for a loss, ‘d’ for a draw.  i.e. this could be passed in for the Sounders July results: “wddwww”
-This function will calculate the total number of points for a team (add 3 for each win, 1 for each tie, 0 for each loss).  
-I have provided a getPointsFromResult function to help you with this, you can call this method for each result.
-Complete orderResults function.  This accepts unlimited team objects { name, results }, and logs the team name & points
-Wrap everything in an IIFE to keep the variables out of the global scope
-Sample output from 
-
-Hints:
-Use string.split to convert a string to an array (use an empty string to separate every single character).  
-So 'wwld'.split('') will give you ['w', 'w', 'l', 'd']
-Use array.forEach with a function to get the number of points from a result and add to the running total
-Use arguments variable in orderResults function to get all of the arguments.
-Use Array.from(arguments) to convert the arguments object to an array
-Use forEach method to run a function on each item in the array
-*/
-
 //given object
 const RESULT_VALUES = {
   w: 3, //win
@@ -43,33 +25,37 @@ const getPointsFromResult = function getPointsFromResult(result) {
 }
 
 // Create getTotalPoints function which accepts a string of results
-// including wins, draws, and losses i.e. 'wwdlw'
+// including wins(w), draws(d), and losses(l) i.e. 'wwdlw'
 // Returns total number of points won
 function getTotalPoints(strResults){
   let totalPoints = 0;
   for(let i=0; i < String(strResults).length; i++){ 
     totalPoints += getPointsFromResult(strResults[i]);
   }
-  //console.log(strResults);
-  console.log(`String results entered is '${strResults}'. Total number of points won is ${totalPoints}`);
+  return(`${totalPoints}`);
 }
 
 // Check getTotalPoints
 console.log(getTotalPoints('wwdl')); // should equal 7
+console.log(getTotalPoints('wwddll')); // should equal 8
 
 
 // create orderTeams function that accepts as many team objects as desired, 
 // each argument is a team object in the format { name, results }
 // i.e. {name: 'Sounders', results: 'wwlwdd'}
 // Logs each entry to the console as "Team name: points"
+function orderTeams(...teamObjs){
+  for (let i=0; i < teamObjs.length; i++){
+    console.log(`${teamObjs[i].name}: ${getTotalPoints(teamObjs[i].results)}`);
+  }   
+}
 
-/*
 // Check orderTeams
 orderTeams(
   { name: 'Sounders', results: 'wwdl' },
   { name: 'Galaxy', results: 'wlld' }
 );
+
 // should log the following to the console:
 // Sounders: 7
 // Galaxy: 4
-*/
